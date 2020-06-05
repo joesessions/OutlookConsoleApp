85d19b3381a5b7af88f80523d677cfe7eeed251c
joesessions <36654608+joesessions@users.noreply.github.com>
2020-06-05 00:48:15 -0700

    First Submission

---
+++ OutlookConsole/GraphClient.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Text.RegularExpressions;
+
+namespace OutlookConsole
+{
+    class GraphClient
+    {
+        private const string ClientId = "api://93e33143-ebea-4267-b9f1-5fec63ab5bd9";
+        private const string AadInstance = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
+        private const string Tenant = "JoeAsDeveloperhotmail.onmicrosoft.com";
+        private const string Resource = "https://microsoft.graph.net";
+        private const string ClientSecret = "Go.74gmOpfhiQk8-agE-R0ZDFwf9O.KOE_";
+
+        private static HttpClient HttpClient = new HttpClient();
+      
+        private const string GetThisUserToken = "EwBgA8l6BAAUO9chh8cJscQLmU+LSWpbnr0vmwwAAVTjC/Fx2kluU3lShq30pvDQdB6a9Ef1DUBVrFWiRB8HtKtF15A4bWt3/iKb7PjH4/4AEHsHeBu62Ue6051/R1ZzqayTiA3dGddjRWT7XJtaEUItaFd6MnEV9eGQ0vtWHprutL19DrYihvbzE6jIPBjWQlxIhXiHuCEPZbLSMjEZmt0f0srec4s8402+rcD0+GYfQSPIPqX/OZx+YdPfO5/YLRO3MWB9+S34XPGBlbtgIGCz7MNZAE37e/4ah6S6r8soKH9zn1k321HQ4/1Kn9z5Ak1FWNspo+cQhhJVct4kORzQXhSW95VxIR+dp4xBjQuGD/mexKtvAbmWEMRhEjUDZgAACIUvq/HPVw/iMAJTBqKpCY8FdHT+L+/73nN2Uph+bPulerh1gy8BxNXkkvvlWpc9JU1fGfTLZhVmJG7NlsMbsRlkotVjkmOWcLRqQNf7rnQv+1NI3889M25Y8FlKSdER9O2JDB8hTsQodd19yRwtiaULzW7KPA8DXdESQ/tCMEWBXSesol3q2esQtzURCvASuEHklraobYl3uFXvAXFawOxV9Xbzlzil9RUH/8481lSz/sa9u21c9cwwzvOGYR/XxCbyFxrXkES5EJRR4ihWqtQtJ629W2M1lkLCvB7Fqk7e8rbcbnLxWsTxMLDZHhYzGMQEhwdO48Exjcm+k2Vh9jS/EZ3UxkiibMddPx8P/ks/DwFgux1Ae7X/8mP3Uuzts4EJjOLPwMMbZ7b7IlpNIYd+BRQjhxYbFnOh7tt9s4qj65qlqK4TvbHPVRLMzbs0vxofrjiqgOjo3AE/5U5gyr5vYuH4M6zGSDb4Q+PD7zugqq6CJ9YhYpbr89uNzfB1SPxF4aDgK03PwjcDIjSanEXaL6uesi8reedZFb6sZlDsjj4cH9YXv8SaOcNnuYknUEgCM76C0TH8L1Bra/48KPy3JbSE5oxKC8c0w9J36zjwayWkM9mZAzJvf26rrLW1DH/DZ+blybV+EtYAZ+tOunDmBpild+6mxtWtyowzCffTC6eEaIyOoMVFO4/oOiCrb6D3c4ZPqi6OrcD5E+J0sCQmojUDtlaAqqibB3YvGWtd/ulXmPmahPa3eXQC";
+        private const string ReadWriteEmailToken = "EwBoA8l6BAAUO9chh8cJscQLmU+LSWpbnr0vmwwAATg5ackd5FfieqBsm2vuEsNgl0LRUO9IGsx/nSKx2YCkVveanN4QZwmB2FhwEXhi0wvwfshhMpstonxYpffWolLIV36D9a8DxIETLcMWPcZB/coKSr7ULCB1jHilJBu1naT4Lf8dt32k+3/8jiAu3rUGYdv+WhzBJKKb1Ia0u+nt7r5Dk7VDpCUCOBfR7brxjvetw4zCJcyvTwvCajiN7PwBxeWg0GFw0z8SBGb/7CEVgN8z3qWHAE47+g6nsrmAaxz6BMwVTy/e/gh0VD7fvXGg6Nli/IWG96hvVe61uRRVdz5S4w/YUGCEGi9KwArQTPb6uTQDPgO5jdfS9FyTxAADZgAACAK3ZyLTTAtuOAIMoqtZXmWt2K+AtOFYsdXS19Nlo0bB0tYGkMxBtplmPwyzRoBTU7DX7lKeG2FpqcVTLFLr1+DLfywRsnvxrLNFX0KVVeUvVpZ2/bLlpVD7I/0H3eVSUA6ErdV61WBwfJV4mUuInzqVzE2wHKcTUSOlkOEw/jXxHFWyf/mEjem4iQxCAhYdLdf2WdpUeT9ye6y5xTC/mpUXmgQonmEGGU8vJb0rWf1uLtEmBBVNNFx4phWyBELLyyCW7oizw1/MZ0GLbAJ3KhwUHuI3LwFYBcb1ZFBS/hJibLduU+fFXUT6prv2pdcQpsfuJ5Hv+8lcRCziyIQ9+KO9ahXKiM79keDHcenmMlkZOiuP/PnPywN1Vq8BTl1Yz2iRYKqfsqFzZtl/wXD+uH1xo0x/539Foq/aVcI3PQQnoPKPmybHPZosUYfo66jJxggouXatqSx0ewTvbyTlBU4I5bYg7x7mKrf30n89ZqVyAz4yWkYeDC7KvBA0ydMPkCUSpv27V/i686BUvV+roy91aFv3NIGWKCc1FxjvJ0aXaikYPtzT2HKwO3sSEToeknv7F4GBrG6J0NNpA3AS+x9A2b75UrTIVgDtOvHekW8fxEflIjY7o/vbxPcjXm5KhUtfw0U+dss/gLx9rhqfWZHqKWOL+xWbmkgayeTj5eQdQkJjPZCfKCq8AwjeSHl3fQLRdxaLUFbWnl9GUABpVTXRfYbYAtFoGZT6jlud20yc4xUzH1UOegVA609Q6oC0Mqr6eAI=";
+
+        public async Task<string> GetThisUser()
+        {
+            string users = null;
+            var uri = "https://graph.microsoft.com/v1.0/me/";
+
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", GetThisUserToken);
+            var getResult = await HttpClient.GetAsync(uri);
+
+            if (getResult.Content != null)
+            {
+                users = await getResult.Content.ReadAsStringAsync();
+            }
+
+            return users;
+        }
+
+        public async Task<List<SimpleEmail>> GetTenEmails()
+        {
+            string emailsJsonString = null;
+            var uri = "https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages";
+
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ReadWriteEmailToken);
+            var getResult = await HttpClient.GetAsync(uri);
+
+            if (getResult.Content != null)
+            {
+                emailsJsonString = await getResult.Content.ReadAsStringAsync();
+            }
+
+            var value = JsonConvert.DeserializeObject<RootObject>(emailsJsonString);
+
+            var retEmails = new List<SimpleEmail>();
+
+            foreach (Value val in value.value)
+            {
+                retEmails.Add(
+                    new SimpleEmail() {
+                        Subject = val.subject,
+                        FromAddress = val.from.emailAddress.Address,
+                        Date = val.sentDateTime,
+                        Id = val.id,
+                        Body = HtmlToPlainText(val.body.content) // Remove the method to get more HTML artifacts.
+                    }
+                );
+            }
+                       
+            return retEmails;
+        }
+
+        public class Value
+        {
+            [JsonProperty("@odata.etag")]
+            public string etag { get; set; }
+            public string accountnumber { get; set; }
+            public string accountid { get; set; }
+            public string subject { get; set; }
+            public From from { get; set; }
+            public DateTime sentDateTime { get; set; }
+            public string id { get; set; }
+            public Body body { get; set; }
+
+        }
+
+        public class Body
+        {
+            public string contentType { get; set; }
+            public string content { get; set; }
+        }
+
+        public class From
+        {
+            [JsonProperty("emailAddress")]
+            public EmailAddress emailAddress { get; set; }
+        }
+
+        public class RootObject
+        {
+            [JsonProperty("@odata.context")]
+            public string context { get; set; }
+            public List<Value> value { get; set; }
+        }
+
+        public class SimpleEmail
+        {
+            public string Subject { get; set; }
+            public string FromAddress { get; set; }
+            public DateTime Date { get; set; }
+            public string Id { get; set; }
+            public string Body { get; set; }
+        }
+
+        private static string HtmlToPlainText(string html)
+        {
+            const string tagWhiteSpace = @"(>|$)(\W|\n|\r)+<";//matches one or more (white space or line breaks) between '>' and '<'
+            const string stripFormatting = @"<[^>]*(>|$)";//match any character between '<' and '>', even when end tag is missing
+            const string lineBreak = @"<(br|BR)\s{0,1}\/{0,1}>";//matches: <br>,<br/>,<br />,<BR>,<BR/>,<BR />
+            var lineBreakRegex = new Regex(lineBreak, RegexOptions.Multiline);
+            var stripFormattingRegex = new Regex(stripFormatting, RegexOptions.Multiline);
+            var tagWhiteSpaceRegex = new Regex(tagWhiteSpace, RegexOptions.Multiline);
+
+            var text = html;
+            //Decode html specific characters
+            text = System.Net.WebUtility.HtmlDecode(text);
+            //Remove tag whitespace/line breaks
+            text = tagWhiteSpaceRegex.Replace(text, "><");
+            //Replace <br /> with line breaks
+            text = lineBreakRegex.Replace(text, Environment.NewLine);
+            //Strip formatting
+            text = stripFormattingRegex.Replace(text, string.Empty);
+
+            return text;
+        }
+
+        public async Task<bool> SendEmail()  // string toEmail, string subject, string body)
+        {
+            var graphClient = new GraphClient();
+
+            var message = new Message
+            {
+                Subject = "Hello Test Email",// subject,
+                Importance = Importance.Low,
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = "Hello World" //body
+                },
+                ToRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "joesessions@gmail.com"// toEmail
+                        }
+                    }
+                }
+            };
+            
+            var uri = "https://graph.microsoft.com/v1.0/me/sendMail";
+            var sendable = new Sendable()
+            {
+                Message = message,
+                //saveToSentItems = false
+            };
+            var json = JsonConvert.SerializeObject(sendable);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ReadWriteEmailToken);
+
+            var getResult = await HttpClient.PostAsync(uri, data);
+
+            return true;
+        }
+
+    }
+
+    public class Sendable
+    {
+        public Message Message { get; set; }
+        public bool saveToSentItems { get; set; }
+    }
+
+
+}
---
+++ OutlookConsole/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using static OutlookConsole.GraphClient;
+
+namespace OutlookConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var graphClient = new GraphClient();
+            bool anotherRequest = true;
+            while (anotherRequest)
+            {
+                Console.WriteLine("Choose a number:");
+                Console.WriteLine("1. Get your info from Microsoft");
+                Console.WriteLine("2. Get your last 10 emails");
+                Console.WriteLine("3. Write an email");
+                ConsoleKeyInfo choice = Console.ReadKey();
+                Console.WriteLine();
+                Console.WriteLine();
+                switch (choice.KeyChar)
+                {
+                    case '1':
+                        Task<string> users = graphClient.GetThisUser();
+                        users.Wait();
+                        Console.WriteLine(users.Result);
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case '2': 
+                        Task<List<SimpleEmail>> emails = graphClient.GetTenEmails();
+                        emails.Wait();
+                        foreach (SimpleEmail email in emails.Result)
+                        {
+                            Console.WriteLine("Subject:   " + email.Subject);
+                            Console.WriteLine();
+                            Console.WriteLine("From:      " + email.FromAddress);
+                            Console.WriteLine();
+                            Console.WriteLine("Date:      " + email.Date);
+                            Console.WriteLine();
+                            Console.WriteLine("Email ID:  " + email.Id);
+                            Console.WriteLine();
+                            Console.WriteLine(email.Body);
+                            Console.WriteLine();
+                            Console.WriteLine();
+                        }
+
+                        Console.ReadLine();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                    case '3':
+                        //Console.Write("To? (enter a valid email address?)");
+                        //string toEmail = Console.ReadLine();
+                        //Console.Write("Subject?");
+                        //string subject = Console.ReadLine();
+                        //Console.WriteLine("Message body?");
+                        //string body = Console.ReadLine();
+                        Task<bool> draftCreated = graphClient.SendEmail();
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        break;
+                }
+                
+            }
+        }
+    }
+}
+
+//get user, after getting token
+//var client = new RestClient("https://graph.microsoft.com/v1.0/me/");
+//client.Timeout = -1;
+//var request = new RestRequest(Method.GET);
+//request.AddHeader("Authorization", "Bearer EwBgA8l6BAAUO9chh8cJscQLmU+LSWpbnr0vmwwAAbeXxyeJGh5SJcmBf+52EI5xj+Ve96BXDR5fqTIwqv63zp9Bv9LQAs2r+eofk6866yBNHv7qs95tMKnN7ypZfV3W8TfzrBbURuufNdhyj/Sg/u9r/NDLgj/LYhfXt7cmCa8VpT6TOM8wq9DYC+1KU8VwdzB+u0PCYt5t8muhg7Jd+n6u9aqqgIyjUkydle0BfEPfVEnYormF4xrhkEx3WFOVjkLqPL0k4pfKCyRSgf+QC9j6LYHygPZDeEkv9wlwoP0Rgb0L1lqGQwQ6y5sFeRVw907NeRWFEAlsg1RnpYJiruwXOhRsMmDPuCS+lsj4rz1Ju2zhf9hZLJZuxC5yb2oDZgAACO/9jqsiP0JAMAL9ZC9jvFAZSXC98pc7QUkCIXyBCxfqxPx9uhALQqOLIu3UXQXnr+SDqrSWEavkSGAPLDQiQ2spYkKkX85u2LsP8IVrC4qnSSEiVY9MeIiCyJmomu1mTRAKJJ8ojc5vnJnxpBQPMUUHcAW6gsqw0KQYD3/dgMBmCatRp5d2bn3rj+v1ekDcJmsaf2QnRwulnmz42a+jgccleCZmK3u3Mbj9K/MGMes4ZHnDmsIy+2bXtah6allgbSqTfLgzubFEnKRWGY6yEHvHbd3rTRy7zorWqkJQHHGHp+s/wob7tOUn9T3+mh5m6eyTuWjWwBUdxL93HN2xQMhfDkWfFGNXd+ADiK2saDEYy5oVNtbdOPXCe1OF+sXWIlxLFw2RtQwGsqXRIeaD0OVDkODuKMfo9IuGa1bBnRq2KVWzWmH2vBPPtyWgBZgw1WSdi/cCSvxoy5KETrI+Flpm/35oyqvTlUEQKad0sWtYwVJCFJTsBWmFrZd54bwD2OdHoUDvoCKU5snvElzpf20ig/K9muZZsb7C2oajPK7b7wmYK9j/gfjMuhRp8YA6C7sjlorAyRQSRasbyKK/uaB6pN6qu9ubmX1CYFpHSvV792wRsSCPDBJMdvFvgJa1xz1baP7zo30D/WX7nJJrlHYM4zZSHQahofM+FD+weaNCRGIQ5IRgh7MaTVLLjO9vmuRoVM9SIwoRZwnDxOzemvCyjXQNk8bMHMSeSCCm4dvZOagHi5eOXR8k9nAC");
+//IRestResponse response = client.Execute(request);
+//Console.WriteLine(response.Content);
+
+
+//Get 10 messages
+//var client = new RestClient("https://graph.microsoft.com/v1.0/me/mailFolders/inbox/messages");
+//client.Timeout = -1;
+//var request = new RestRequest(Method.GET);
+//request.AddHeader("Authorization", "Bearer EwBgA8l6BAAUO9chh8cJscQLmU+LSWpbnr0vmwwAAeDUcwVPwJ5Q5Elbp2Wt2bholzpKhB+uuYZlneGheJ+SJqL1pyo667vCKc2/1yGl/Y81V+LxlHPtjNnMU41kfQd4M3krXnBIUCj4jw51Rj3wNOOmma8reSKLELIHIdkZLQLRazxUNDk/BkOhJvs5kUC81eWA+UNrGa3MJml3DIKSjzr1wexaAAlw0jij0bg51l6YwurvlHM5VpHj6BRmNEI3NKQtVgl9JCQNYyw1485jW/gJsW5xUZyvyg84j87/b6BcUjlYeCcbQLimn1uEye3FZYNz0ffoF2zCdsNqCk29qNCqwDAAIrypHNDZ5hhdLYfCs10I7UXRrVpuj8N5J2QDZgAACKNo2YZONdfUMAKUxIoi8Qs8P67iEB0eIY6zM/fvIKZTI3tRN9WQiEZn8kNeY4vQn5xIeTdpjv5n9Vhh4OBB1w1y/ZvjYPlQVZEc6CiQZRHT8KKoaUyuLKNrTajqDmy1HNM5jt2mBBNBIcEMfZ4hcSwdQpOBvzNTDfcO7YXw07i4BBejjq2Z2P+lF0XhIn7ict+TNpqLxPm5/HdWySCAlkjHzDePeI80zd0AyRU1X5FeGxv+DkEgm3F777TqstPEcOFQ9RJajBB24dLXBtGfhz0g2r1D6iDMXuxmzG1CSUSlWpmuyW1Pvvo2QxzZNobd7VAQUlPTZGmxmw/LLG+Ml21m5RczVD6tc0iykKzGof2+R+QYlPoQCfo9S61W+56HwzFRzg8OkI80JfY+T2nE/drgFj1DAkXkh7mNUfmuzD1o9h+fDoRc6esKY62Tyqxm25ukuSmwZTFsT4iNek6X8jFylrG7H996KfCK1zgAZDH+z0k01skBwl7euCYCNL7t2ns7Q+f2DdPXM5zF4td07vUXOev1tL7QEZ/Z3Bukp4v3ORk9vWGq9JxOCWTmgWJL5gmMAaPYC2S2FwFAc797wUx8Ga9INCB93c1GZObYPFiX9O/ysFLZFAN4Ub+E/IiX8Et/S9YxzVSVekZhgiazL0HspT+fqpuDewIWqtx7umQzOjOn2Q9DVm0FKtSHxmhBP22in4Jr3uwlcPnB0XhlVbEbzPFLcMiP8cJUulmw0TaLkNPwqwpXw/fjR3QC");
+//IRestResponse response = client.Execute(request);
+//Console.WriteLine(response.Content);
+
+//
