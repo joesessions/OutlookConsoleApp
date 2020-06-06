321b2066d711c4323068811d71c769ce37382a56
joesessions <36654608+joesessions@users.noreply.github.com>
2020-06-06 10:21:14 -0700

    New root location

--- OutlookConsole/Program.cs
+++ OutlookConsole/Program.cs
@@ -1,9 +1,5 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using static OutlookConsole.GraphClient;
 
